db6972cc003e0d756a53c6f8c723b840d2ad97cb
Saulo Filho <32524964+saulofilho@users.noreply.github.com>
2020-04-23 23:05:40 -0300

    [FIX] profile avatar url

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://deploy-nodejs.saulofilho.com/' });
+const api = axios.create({ baseURL: 'https://deploy-nodejs.saulofilho.com' });
 
 export default api;
